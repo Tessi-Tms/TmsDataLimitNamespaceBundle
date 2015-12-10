34b3804e7ae1f3d7bdeb615e33aa9e268aea2deb
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2015-12-10 16:23:42 +0100

    Implements Elactica in ElasticSearchDataProvider. Work on ElasticSearchDataProvider & add tests

--- DataProvider/ElasticSearchDataProvider.php
+++ DataProvider/ElasticSearchDataProvider.php
@@ -2,15 +2,61 @@
 
 namespace Tms\DataLimitNamespaceBundle\DataProvider;
 
+use Elastica\Client;
+use Elastica\Filter\BoolAnd;
+use Elastica\Query;
+use Elastica\Query\QueryString;
+use Elastica\Query\Match;
+use Elastica\Filter\Term;
+use Elastica\Search;
+use Elastica\Type\Mapping;
+
 class ElasticSearchDataProvider implements DataProviderInterface
 {
+    private $client;
+    private $esIndex;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->client = new Client(array('localhost', 9200));
+        $this->esIndex = $this->client->getIndex('limit_data');
+
+        // Checks if the index is already created
+        if (!$this->esIndex->exists()) {
+            $this->esIndex->create();
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getCount(array $data, array $keys, $namespace)
     {
-        // Reorder keys
-        // Return the count of hash based on given namespace & data
+        if (!$this->hasNamespace($namespace)) {
+            $this->defineMapping($namespace);
+        }
+
+        $values = $this->getValues($data, $keys);
+        $hash = $this->getHash($values);
+
+        $esTerm = new Term();
+        $esFilterAnd = new BoolAnd();
+
+        $esTerm->setTerm('hash', $hash);
+
+        foreach ($keys as $key => $value) {
+            $esTerm->setTerm('keys', $value);
+            $esFilterAnd->addFilter($esTerm);
+        }
+
+        $esQuery = new Query();
+        $esQuery->setFields(['hash', 'keys']);
+        $esQuery->setPostFilter($esFilterAnd);
+
+        return $this->esIndex->getType($namespace)->search($esQuery)->count();
     }
 
     /**
@@ -18,10 +64,7 @@ class ElasticSearchDataProvider implements DataProviderInterface
      */
     public function hasNamespace($namespace)
     {
-        // Check in elastic search if namespace exist
-        // Return true if exist
-
-        return false;
+        return $this->esIndex->getType($namespace)->exists();
     }
 
     /**
@@ -34,5 +77,63 @@ class ElasticSearchDataProvider implements DataProviderInterface
         // count lower than limit return false
 
         return true;
+    }
+
+    /**
+     * Define a mapping
+     *
+     * @param string $namespace
+     */
+    private function defineMapping($namespace)
+    {
+        $mapping = new Mapping();
+        $mapping->setType($this->esIndex->getType($namespace));
+        $mapping->setProperties(array(
+            'hash'=> array('type' => 'string'),
+            'keys'=> array('type' => 'string'),
+        ));
+        $mapping->send();
+    }
+
+    /**
+     * Get the values of the given data with the given keys
+     *
+     * @param array $data
+     * @param array $keys
+     *
+     * @return array
+     */
+    public function getValues(array $data, array $keys)
+    {
+        $result = array();
+        sort($keys);
+
+        foreach($keys as $key) {
+            $result[] = $data[$key];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get hash based on given data
+     *
+     * @param $data
+     *
+     * @return string
+     */
+    public function getHash($data)
+    {
+        if (is_string($data)) {
+            return md5($data);
+        } elseif (is_array($data)) {
+            $string  = "";
+
+            foreach($data as $value) {
+                $string .= $value;
+            }
+
+            return md5($string);
+        }
     }
 }
--- Tests/DataProvider/ElasticSearchDataProviderTest.php
+++ Tests/DataProvider/ElasticSearchDataProviderTest.php
@@ -11,6 +11,22 @@ class ElasticSearchDataProviderTest extends \PHPUnit_Framework_TestCase
     {
         $elasticSearchDataProvider = new ElasticSearchDataProvider();
 
-        $this->assertEquals(false, $elasticSearchDataProvider->hasNamespace('titi'));
+        $this->assertEquals(false, $elasticSearchDataProvider->hasNamespace('brahim'));
+        $this->assertEquals(true, $elasticSearchDataProvider->hasNamespace('op2'));
+    }
+
+    public function testGetCount()
+    {
+        $elasticSearchDataProvider = new ElasticSearchDataProvider();
+
+        $this->assertEquals(0, $elasticSearchDataProvider->getCount(
+            array(
+                'name'    => 'Brahim',
+                'address' => '1 rue Peter Parker',
+                'age'     => 25
+            ),
+            array('name', 'address'),
+            'test'
+        ));
     }
 }
