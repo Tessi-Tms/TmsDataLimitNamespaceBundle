1d88b0648d26a13fee86c99a3ac60da1150dfda3
Brahim BOUKOUFALLAH <boukoufallah.brahim@gmail.com>
2017-04-20 16:34:32 +0200

    Remove static function and add dynamically host and port.

--- Tests/DataProvider/ElasticSearchDataProviderTest.php
+++ Tests/DataProvider/ElasticSearchDataProviderTest.php
@@ -3,149 +3,182 @@
 namespace Tms\Bundle\DataLimitNamespaceBundle\Tests\DataProvider;
 
 use Elastica\Client;
+use Elastica\Connection;
 use Tms\Bundle\DataLimitNamespaceBundle\DataProvider\ElasticSearchDataProvider;
 
+/**
+ * Class ElasticSearchDataProviderTest
+ */
 class ElasticSearchDataProviderTest extends \PHPUnit_Framework_TestCase
 {
     const ELASTIC_SEARCH_INDEX_NAME = 'tms_limit_test';
 
+    private $client;
+    private $provider;
+
+    /**
+     * Set up
+     */
     public function setUp()
     {
-        $client = self::buildClient();
+        $this->client = new Client(array('host' => $this->getHost(), 'port' => $this->getPort()));
+        $this->provider = new ElasticSearchDataProvider(
+            $this->client,
+            self::ELASTIC_SEARCH_INDEX_NAME,
+            true
+        );
+    }
+
+    /**
+     * Tear down
+     */
+    public function tearDown()
+    {
         // Delete the index if already exists
-        $index = $client->getIndex(self::ELASTIC_SEARCH_INDEX_NAME);
+        $index = $this->client->getIndex(self::ELASTIC_SEARCH_INDEX_NAME);
         if ($index) {
             $index->delete();
         }
     }
 
-    public static function buildClient()
+    /**
+     * Check if ES_HOSt env variable exists and returns its. Returns default host otherwise
+     *
+     * @return string
+     */
+    protected function getHost()
     {
-        return new Client(array('host' => 'localhost', 'port' => 9200));
+        return getenv('ES_HOST') ?: Connection::DEFAULT_HOST;
     }
 
-    public static function buildProvider()
+    /**
+     * Check if ES_PORT env variable exists and returns its. Returns default port otherwise
+     *
+     * @return integer
+     */
+    protected function getPort()
     {
-        $provider = new ElasticSearchDataProvider(
-            self::buildClient(),
-            self::ELASTIC_SEARCH_INDEX_NAME,
-            true
-        );
-
-        return $provider;
+        return getenv('ES_PORT') ?: Connection::DEFAULT_PORT;
     }
 
+    /**
+     * Test hasNamespace
+     */
     public function testHasNamespace()
     {
-        $provider = self::buildProvider();
         $data = array('key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3');
 
         // Has namespace
-        $this->assertFalse($provider->hasNamespace('has_dummy_namespace'));
+        $this->assertFalse($this->provider->hasNamespace('has_dummy_namespace'));
 
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key1', 'key2'),
             'has_dummy_namespace'
         );
 
-        $this->assertTrue($provider->hasNamespace('has_dummy_namespace'));
+        $this->assertTrue($this->provider->hasNamespace('has_dummy_namespace'));
     }
+
+    /**
+     * Test store
+     */
 
     public function testStore()
     {
         $data = array('key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3');
 
-        $provider = self::buildProvider();
-
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key1', 'key2'),
             'dummy_namespace'
         );
-        $this->assertEquals(1, $provider->getCount(
+        $this->assertEquals(1, $this->provider->getCount(
             $data,
             array('key1', 'key2'),
             'dummy_namespace'
         ));
 
         // Inverse keys
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key2', 'key1'),
             'dummy_namespace'
         );
-        $this->assertEquals(2, $provider->getCount(
+        $this->assertEquals(2, $this->provider->getCount(
             $data,
             array('key2', 'key1'),
             'dummy_namespace'
         ));
 
         // Change keys
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key1', 'key3'),
             'dummy_namespace'
         );
-        $this->assertEquals(1, $provider->getCount(
+        $this->assertEquals(1, $this->provider->getCount(
             $data,
             array('key1', 'key3'),
             'dummy_namespace'
         ));
 
         // Change namespace
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key1', 'key2'),
             'dummy_namespace_2'
         );
-        $this->assertEquals(1, $provider->getCount(
+        $this->assertEquals(1, $this->provider->getCount(
             $data,
             array('key1', 'key2'),
             'dummy_namespace_2'
         ));
     }
 
+    /**
+     * Test getCount
+     */
     public function testGetCount()
     {
         $data = array('key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3');
 
-        $provider = self::buildProvider();
         for($i = 0; $i < 200; $i++) {
-            $provider->store(
+            $this->provider->store(
                 $data,
                 array('key1', 'key2'),
                 'get_count_dummy_namespace'
             );
         }
 
-        $this->assertEquals(200, $provider->getCount(
+        $this->assertEquals(200, $this->provider->getCount(
             $data,
             array('key1', 'key2'),
             'get_count_dummy_namespace'
         ));
     }
 
+    /**
+     * Test isLimitReached
+     */
     public function testIsLimitReached()
     {
         $data = array('key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3');
 
-        $provider = self::buildProvider();
-
-        $this->assertFalse($provider->isLimitReached(
+        $this->assertFalse($this->provider->isLimitReached(
             $data,
             array('key1', 'key2'),
             'limit_reached_dummy_namespace',
             1
         ));
 
-        $provider->store(
+        $this->provider->store(
             $data,
             array('key1', 'key2'),
             'limit_reached_dummy_namespace'
         );
 
-        $this->assertTrue($provider->isLimitReached(
+        $this->assertTrue($this->provider->isLimitReached(
             $data,
             array('key1', 'key2'),
             'limit_reached_dummy_namespace',
@@ -153,14 +186,14 @@ class ElasticSearchDataProviderTest extends \PHPUnit_Framework_TestCase
         ));
 
         for($i = 0; $i < 29; $i++) {
-            $provider->store(
+            $this->provider->store(
                 $data,
                 array('key1', 'key2'),
                 'limit_reached_dummy_namespace'
             );
         }
 
-        $this->assertTrue($provider->isLimitReached(
+        $this->assertTrue($this->provider->isLimitReached(
             $data,
             array('key1', 'key2'),
             'limit_reached_dummy_namespace',
